2ad4d542b6e252a128af427be275974fe0733385
dannyardila <dannyardila059@gmail.com>
2023-02-17 10:01:52 -0500

    se agrego diagrama de flujo y README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # llamada_telefonica
 programa para calcular el costo de una llamada telefónica 
+
+ingresar el tiempo de duracion de una llamada telefonica y determinar la cantidad de pagar, deacuerdo con lo siguiente:
+
+- toda llamada que dure 3 minutos o menos tiene un costo de $300
+- cada minuto adicional cuesta $50
---
+++ llamada.py
@@ -0,0 +1,19 @@
+# programa para calcular el costo de una llamada telefónica 
+
+# toda llamada que dure 3 minutos o menos tiene un costo de $300
+# cada minuto adicional cuesta $50
+
+print("------------------------------------------")
+print("------------COSTO DE LLAMADA--------------")
+print("------------------------------------------")
+
+# input 
+
+
+# processing 
+
+
+# output 
+print("-------------------------------------------")
+print("---------------RESULTADOS------------------")
+print("-------------------------------------------")
